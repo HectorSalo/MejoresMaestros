8ba6a9e5c380a591f779285ea95fe1789acb67c0
Prueba <hectorsalomonchirinos@gmial.com>
2018-12-28 17:03:35 -0400

    verpub

---
+++ app/src/main/java/com/example/chirinos/mejoresmaestros/VerActivity.java
@@ -0,0 +1,13 @@
+package com.example.chirinos.mejoresmaestros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver);
+    }
+}
